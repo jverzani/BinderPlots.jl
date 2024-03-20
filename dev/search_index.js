865c162599b53b2974937513240fbf067bce2a72
Documenter.jl <documenter@juliadocs.github.io>
2024-03-20 15:36:17 +0000

    build based on 68c8528

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BinderPlots","category":"page"},{"location":"#BinderPlots","page":"Home","title":"BinderPlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BinderPlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BinderPlots]","category":"page"}]
+}
