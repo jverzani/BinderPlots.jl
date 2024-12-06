c7603fc7c909bf02be5b0e1f31969ca8fa50bc4c
jverzani <jverzani@gmail.com>
2024-12-06 16:20:49 -0500

    handle struct type

--- src/utils.jl
+++ src/utils.jl
@@ -302,7 +302,9 @@ rgb(r::_RGB, α=1.0) = _RGB(r.r,r.g,r.b,α)
 rgb(r::_RGB, ::Nothing) = r
 rgb(::Nothing, ::Any) = nothing
 
-PlotlyLight.StructTypes.StructType(::Type{_RGB}) = PlotlyLight.StructTypes.StringType()
+PlotlyLight.JSON3.StructTypes.StructType(::Type{_RGB}) =
+    PlotlyLight.JSON3.StructTypes.StringType()
+
 function Base.string(c::_RGB)
     (;r,g,b,α) = c
     "rgba($r,$g,$b,$α)"
